bf36183a48c01ae90e85adb078f345b07a7d6125
Josélio A de Barros <p000jbarros@prservicos.com.br>
2018-11-13 09:02:04 -0200

    project restructuring

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+
+import { SharedModule } from './../../shared/shared.module';
 
 import { EntriesRoutingModule } from './entries-routing.module';
 
@@ -13,10 +13,9 @@ import { IMaskModule } from 'angular-imask';
 @NgModule({
 	declarations: [EntryListComponent, EntryFormComponent],
 	imports: [
+		SharedModule,
 		CalendarModule,
 		IMaskModule,
-		CommonModule,
-		ReactiveFormsModule,
 		EntriesRoutingModule
 	]
 })
