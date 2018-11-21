c143b875bf53b6020d6e6c4258a5cbf7983f5058
Josélio A de Barros <p000jbarros@prservicos.com.br>
2018-11-21 08:55:04 -0200

    improving EntryService with BaseResourceService

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,38 +1,19 @@
+import { Injectable, Injector } from '@angular/core';
+
+import { BaseResourceService } from '../../../shared/services/base-resource.service';
 import { CategoryService } from './../../categories/shared/category.service';
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-
-import { Observable, throwError } from 'rxjs';
-import { map, catchError, flatMap } from 'rxjs/operators';
-
 import { Entry } from './entry.model';
+
+import { Observable } from 'rxjs';
+import { map, catchError, flatMap } from 'rxjs/operators';
 
 @Injectable({
 	providedIn: 'root'
 })
-export class EntryService {
+export class EntryService extends BaseResourceService<Entry> {
 
-	private apiPath = 'api/entries';
-
-	constructor(
-		private http: HttpClient,
-		private categoryService: CategoryService
-	) { }
-
-	getAll(): Observable<Entry[]> {
-		return this.http.get(this.apiPath).pipe(
-			catchError(this.handleError),
-			map(this.jsonDataToEntries)
-		);
-	}
-
-	getById(id: number): Observable<Entry> {
-		const url = `${this.apiPath}/${id}`;
-
-		return this.http.get(url).pipe(
-			catchError(this.handleError),
-			map(this.jsonDataToEntry)
-		);
+	constructor(protected injector: Injector, private categoryService: CategoryService) {
+		super('api/entries', injector);
 	}
 
 	create(entry: Entry): Observable<Entry> {
@@ -42,7 +23,7 @@ export class EntryService {
 
 				return this.http.post(this.apiPath, entry).pipe(
 					catchError(this.handleError),
-					map(this.jsonDataToEntry)
+					map(this.jsonDataToResource)
 				);
 			})
 		);
@@ -63,18 +44,9 @@ export class EntryService {
 		);
 	}
 
-	delete(id: number): Observable<any> {
-		const url = `${this.apiPath}/${id}`;
+	// PROTECTED METHODS
 
-		return this.http.delete(url).pipe(
-			catchError(this.handleError),
-			map(() => null)
-		);
-	}
-
-	// PRIVATE METHODS
-
-	private jsonDataToEntries(jsonData: any[]): Entry[] {
+	protected jsonDataToResources(jsonData: any[]): Entry[] {
 		const entries: Entry[] = [];
 
 		jsonData.forEach(element => {
@@ -85,12 +57,7 @@ export class EntryService {
 		return entries;
 	}
 
-	private jsonDataToEntry(jsonData: any): Entry {
+	protected jsonDataToResource(jsonData: any): Entry {
 		return Object.assign(new Entry(), jsonData);
-	}
-
-	private handleError(error: any): Observable<any> {
-		console.log('ERRO NA REQUISIÇÃO => ', error);
-		return throwError(error);
 	}
 }
