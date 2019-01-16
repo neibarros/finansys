d5e0a057d1a55c8186105a264260023c446fd177
Nei Barros <nei_webdev@hotmail.com>
2019-01-16 15:58:29 -0200

    Adding FormFieldErrorComponent to Forms

--- src/app/shared/components/page-header/page-header.component.ts
+++ src/app/shared/components/page-header/page-header.component.ts
@@ -7,10 +7,10 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PageHeaderComponent implements OnInit {
 
-	@Input('page-title') pageTitle: string;
-	@Input('button-class') buttonClass: string;
-	@Input('button-text') buttonText: string;
-	@Input('button-link') buttonLink: string;
+	@Input('page-title') pageTitle: string; // tslint:disable-line:no-input-rename
+	@Input('button-class') buttonClass: string; // tslint:disable-line:no-input-rename
+	@Input('button-text') buttonText: string; // tslint:disable-line:no-input-rename
+	@Input('button-link') buttonLink: string; // tslint:disable-line:no-input-rename
 
 	constructor() { }
 
